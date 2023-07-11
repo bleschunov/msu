c9e06b14a26da76c8e394568c470f90d50697eb7
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-07-11 08:03:55 +0300

    add comment and model-name

--- src/chain.py
+++ src/chain.py
@@ -7,11 +7,12 @@ from langchain.chat_models import ChatOpenAI
 db = SQLDatabase.from_uri(os.getenv("DB_URI"), include_tables=["test"])
 
 # Создаём ЛЛМ–модель для работы цепочки для работы с SQL
-# -1 указывает, что лимит токенов будет подстраиватья под запрос динамически
-llm = ChatOpenAI(temperature=0, verbose=False, max_tokens=512)
+# max_tokens=-1 указывает, что лимит токенов будет подстраиватья под запрос динамически
+llm = ChatOpenAI(temperature=0, verbose=False, max_tokens=512, model_name="gpt-3.5-turbo-16k")
 
 # Создаём цепочку для работы с SQL
 # use_query_checker используем для обработки неправильно составленных SQL запросов
+# use_query_checker=False, потому что True с моделью на 16К токенов ломает чейн
 # Используем кастомный промпт, чтобы указать в нём особенности нашей таблицы
 db_chain = SQLDatabaseChain.from_llm(
     llm, db, use_query_checker=False
