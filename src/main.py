efee55659130e00feb38b499ce55194879b0a336
nouhadonosor <nouhadonosor@gmail.com>
2023-07-12 18:04:59 +0300

    Rework Optimised streamlit ui/ux

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import uuid
 from dataclasses import dataclass
+from typing import List
 
 import streamlit as st
 from streamlit_chat import message
 
-from chain import db_chain
+
 from custom_memory import custom_memory
 from sql_database_chain_executor import SQLDatabaseChainExecutor
 
-executor = SQLDatabaseChainExecutor(db_chain, custom_memory, debug=False)
+SESS_STATE = st.session_state
 messages_container = st.container()
 
 
@@ -16,50 +17,64 @@ messages_container = st.container()
 class Message:
     text: str
     is_user: bool
+    key: str = None
+
+    def __post_init__(self):
+        self.key = uuid.uuid4().hex
+
+    def make_st_message(self, key=None):
+        message(self.text, self.is_user, key=self.key if not key else key)
 
 
-def print_messages():
-    for msg in st.session_state.msg_list:
-        message(msg.text, msg.is_user, key=uuid.uuid4().hex)
+def reprint_messages_from_msg_list():
+    for n, msg in enumerate(st.session_state.msg_list):
+        msg.make_st_message(n)
+
+
+def initialize():
+    if not SESS_STATE:
+        # slow imports made here
+        from chain import db_chain
+        SESS_STATE.sql_chain_executor = SQLDatabaseChainExecutor(
+            db_chain, custom_memory, debug=False)
+
+        st.session_state.msg_list: List[Message] = []
+        greeting_message = Message('Привет! Какой у вас запрос?', False)
+        st.session_state.msg_list.append(greeting_message)
 
 
 def reset():
-    executor.reset()
+    SESS_STATE.sql_chain_executor.reset()
 
     reset_message = Message("Контекст сброшен", False)
     st.session_state.msg_list.append(reset_message)
 
-    with messages_container:
-        message(reset_message.text, reset_message.is_user, key=uuid.uuid4().hex)
+
+def on_input_change():
+    query = st.session_state.user_input
+    if query:
+        query_message = Message(query, True)
+        st.session_state.msg_list.append(query_message)
+
+        with messages_container:
+            query_message.make_st_message()
+
+        st.session_state["input_text"] = ""
+
+        answer = SESS_STATE.sql_chain_executor.run(query)
+        answer_message = Message(answer, False)
+        st.session_state.msg_list.append(answer_message)
 
 
-if "msg_list" not in st.session_state:
-    st.session_state.msg_list = []
-
-if len(st.session_state.msg_list) == 0:
-    st.session_state["msg_list"].append(Message('Привет! Какой у вас запрос?', False))
+initialize()
 
 with messages_container:
-    print_messages()
+    reprint_messages_from_msg_list()
 
-query = st.text_input('Ваш запрос', '')
+with st.container():
+    st.text_input('Ваш запрос', '',
+                  on_change=on_input_change, key='user_input')
 
-if query:
-    query_message = Message(query, True)
-    st.session_state.msg_list.append(query_message)
-
-    with messages_container:
-        message(query_message.text, query_message.is_user, key=uuid.uuid4().hex)
-
-    st.session_state["input_text"] = ""
-
-    answer = executor.run(query)
-    answer_message = Message(answer, False)
-    st.session_state.msg_list.append(answer_message)
-
-    with messages_container:
-        message(answer_message.text, answer_message.is_user, key=uuid.uuid4().hex)
-
-st.button("Сбросить контекст", on_click=reset)
-
-st.write("История сообщений: " + str(executor.get_chat_history_size()) + " токенов из ~16K")
+    st.button("Сбросить контекст", on_click=reset)
+    st.write("История сообщений: " +
+             str(SESS_STATE.sql_chain_executor.get_chat_history_size()) + " токенов из ~16K")
