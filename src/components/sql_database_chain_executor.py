90e8e48d53f75400ff2aa62d4da84af6f07fbd5c
Dmitry Bleschunov <bleshunov5b@yandex.ru>
2023-07-20 15:19:38 +0300

    refactoring

--- src/components/sql_database_chain_executor.py
+++ src/components/sql_database_chain_executor.py
@@ -2,37 +2,35 @@ import json
 import logging
 import pandas as pd
 import langchain
-import re
-from typing import Dict, List, Tuple
+import dataclasses
 
-from dataclasses import dataclass
 from langchain import SQLDatabaseChain
 from components.custom_memory import CustomMemory, HumanMessage, AiMessage
 
 
-@dataclass
+@dataclasses.dataclass
 class SQLDatabaseChainExecutor:
     db_chain: SQLDatabaseChain
     memory: CustomMemory
-    chain_answer: dict = {}
+    chain_answer: dict = dataclasses.field(default_factory=dict)
     debug: bool = False
     langchain_debug: bool = False
     verbose: bool = False
     return_intermediate_steps: bool = False
+    last_intermediate_steps: list[dict | str] = None
 
     def __post_init__(self):
         langchain.debug = self.langchain_debug
         self.db_chain.verbose = self.verbose
         self.db_chain.return_intermediate_steps = self.return_intermediate_steps
-        self.last_intermediate_steps = None
 
     def run(self, query):
         query_with_chat_history = self.memory.get_memory() + query
         try:
             if self.return_intermediate_steps:
-                r = self.db_chain(query_with_chat_history)
-                chain_answer = r.get("result", None)
-                self.last_intermediate_steps = r.get("intermediate_steps", None)
+                db_chain_response = self.db_chain(query_with_chat_history)
+                chain_answer = db_chain_response.get("result", None)
+                self.last_intermediate_steps = db_chain_response.get("intermediate_steps", None)
             else:
                 chain_answer = self.db_chain.run(query_with_chat_history)
         except Exception as e:
@@ -59,31 +57,31 @@ class SQLDatabaseChainExecutor:
 
         return self
 
-    def get_answer(self) -> str | None:
+    def get_answer(self) -> dict:
         if isinstance(self.chain_answer, dict):
             return self.chain_answer.get("Answer")
         else:
             return self.chain_answer
 
     def get_df(self) -> pd.DataFrame | None:
-        sqlres = (
+        sql_result = (
             self.chain_answer.get("SQLResult")
             if isinstance(self.chain_answer, dict)
             else None
         )
         return (
-            pd.DataFrame(sqlres)
-            if sqlres and isinstance(sqlres, (dict, list))
+            pd.DataFrame(sql_result)
+            if sql_result and isinstance(sql_result, (dict, list))
             else None
         )
 
-    def get_all(self) -> Tuple[str | None, pd.DataFrame | None]:
+    def get_all(self) -> tuple[dict, pd.DataFrame | None]:
         return self.get_answer(), self.get_df()
 
     def get_chat_history_size(self) -> int:
         return self.db_chain.llm_chain.llm.get_num_tokens(self.memory.get_memory())
 
-    def get_last_intermediate_steps(self) -> List[Dict | str]:
+    def get_last_intermediate_steps(self) -> list[dict | str]:
         return self.last_intermediate_steps
 
     def reset(self) -> None:
